00cbd40f1ad272ded11864b164b9b4b83811abea
Muhammad Sarman Noorlah <sarmanbb@gmail.com>
2022-06-15 22:45:20 +0800

    update

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Events\BedPushed;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+/* |-------------------------------------------------------------------------- | Web Routes |-------------------------------------------------------------------------- | | Here is where you can register web routes for your application. These | routes are loaded by the RouteServiceProvider within a group which | contains the "web" middleware group. Now create something great! | */
 
 
 // Route::get('/', [App\Http\Controllers\FrontendController::class , 'index'])->name('/');
@@ -26,18 +17,18 @@ Auth::routes([
     'register' => false
 ]);
 
-Route::middleware(['auth'])->group(function () {
-Route::get('/home', [App\Http\Controllers\HomeController::class , 'index'])->name('home');
+Route::middleware(['auth'])->group(function () {    Route::get('/home', [App\Http\Controllers\HomeController::class , 'index'])->name('home');
 
     Route::resource('categories', App\Http\Controllers\CategoryController::class);
     Route::resource('tags', App\Http\Controllers\TagController::class);
 
-        Route::resource('kelas', App\Http\Controllers\KelasController::class);
+    Route::resource('kelas', App\Http\Controllers\KelasController::class);
 
     Route::resource('kamar', App\Http\Controllers\KamarController::class);
+    Route::resource('catkamar', App\Http\Controllers\CatKamarController::class);
     Route::resource('user', App\Http\Controllers\UserController::class);
-    Route::get('profil', [App\Http\Controllers\ProfilController::class, 'edit'])->name('profile.edit');
-    Route::put('profil', [App\Http\Controllers\ProfilController::class, 'update'])->name('profile.update');
+    Route::get('profil', [App\Http\Controllers\ProfilController::class , 'edit'])->name('profile.edit');
+    Route::put('profil', [App\Http\Controllers\ProfilController::class , 'update'])->name('profile.update');
 
     // Manage Posts
     Route::get('posts/trash', [App\Http\Controllers\PostController::class , 'trash'])->name('posts.trash');
@@ -45,10 +36,7 @@ Route::get('/home', [App\Http\Controllers\HomeController::class , 'index'])->nam
     Route::delete('posts/{id}/delete-permanent', [App\Http\Controllers\PostController::class , 'deletePermanent'])->name('posts.deletePermanent');
     Route::resource('posts', App\Http\Controllers\PostController::class);
 
-
-Route::get('post/{slug}', [App\Http\Controllers\FrontController::class, 'show'])->name('show');
-Route::get('category/{category:slug}', [App\Http\Controllers\FrontController::class, 'category'])->name('category');
-Route::get('tag/{tag:slug}', [App\Http\Controllers\FrontController::class, 'tag'])->name('tag');
+    Route::get('post/{slug}', [App\Http\Controllers\FrontController::class , 'show'])->name('show');    Route::get('category/{category:slug}', [App\Http\Controllers\FrontController::class , 'category'])->name('category');    Route::get('tag/{tag:slug}', [App\Http\Controllers\FrontController::class , 'tag'])->name('tag');
 });
 
 Route::get('/test', function () {
