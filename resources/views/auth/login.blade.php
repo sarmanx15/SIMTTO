815e9132e81b753ef23af80100f7d2e715843140
comp-iki <67246778+comp-iki@users.noreply.github.com>
2022-06-28 20:03:47 +0800

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="description" content="Sistem Informasi Tempat Tidur RSUD Abdoel Wahab Sjahranie">
-    <meta name="author" content="KiMan studio">
+    <meta name="author" content="Instalasi PDE">
 
     <title>
         @if (isset($title))
